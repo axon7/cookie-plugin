c418dd15a955751f40ad4d56b4ea21a80932fad2
Kuba <kubaz9991@gmail.com>
2019-09-17 19:09:27 +0200

    Remove unnecessary comments

--- cookiePlugin.js
+++ cookiePlugin.js
@@ -7,7 +7,7 @@ const createCookieBanner = () => {
   const body = document.querySelector("body");
   body.appendChild(cookieBox);
 };
-
+//create cookie (cancel or accept) based on the clicked button
 const createCookie = type => {
   let expiresDate = new Date();
   expiresDate.setTime(expiresDate.getTime() + 24 * 60 * 60 * 1000); //add 24 hours (in miliseconds) to expires date
@@ -19,24 +19,10 @@ const createCookie = type => {
 };
 
 window.onload = function() {
-  //when window loads it checks if cookie exists. if not then visibility should be true
-
+  //when window loads it checks if cookie exists.
+  //if not then cookie should pop-up and disable scrolling
   if (document.cookie.indexOf("GDPR_agreement=") < 0) {
     document.body.classList.toggle("disablescroll");
     createCookieBanner();
   }
 };
-
-//create cookie box
-// when accept => create cookie and save in browser for 24h
-//when
-
-// ◦ Musi uruchomić się jak najszybciej po odczytaniu przez przeglądarkę.
-// ◦ Musi wyświetlić box informacyjny na środku strony o tytule “GDPR consent”.
-// jednocześnie wyłączając możliwość scrollowania (tak żeby zawsze był
-// widoczny).
-// ◦ Po kliknięciu w Accept lub Cancel plugin musi zapisać informację w
-// przeglądarce czy użytkownik wyraził zgodę (Accept) czy nie (Cancel), a box
-// powinien zniknąć włączając przy tym ponownie scrolla.
-// ◦ Po odświeżeniu strony plugin powinien wyświetlić ponownie box informacyjny,
-// jednak dopiero po 24 godzinach od poprzedniej decyzji użytkownika.
